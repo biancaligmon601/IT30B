b461dc19286407f141bf841c73fe3af1525a53e0
biancaligmon601 <biancaligmon93@gmail.com>
2026-09-22 08:48:15 +0800

    Added backup 09_22_2026_it30_lab_db.sql

---
+++ backup/09_22_2026_it30b_lab_db.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 22, 2026 at 02:44 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `it30b_lab_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `book_id` int(11) NOT NULL,
+  `book_title` varchar(100) NOT NULL,
+  `book_author` varchar(100) NOT NULL,
+  `book_category` varchar(100) NOT NULL,
+  `book_created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrow`
+--
+
+CREATE TABLE `borrow` (
+  `borrow_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `borrow_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `borrow_return_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `student_id` int(11) NOT NULL,
+  `student_first_name` varchar(255) NOT NULL,
+  `student_last_name` varchar(255) NOT NULL,
+  `student_course` varchar(255) NOT NULL,
+  `student_created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`student_id`, `student_first_name`, `student_last_name`, `student_course`, `student_created_at`) VALUES
+(1, 'Bixxa', 'Viodor', 'BSIT', '2026-08-24 20:30:20'),
+(2, 'Biyya', 'Ligmon', 'BSBA', '2026-08-24 20:30:27'),
+(3, 'Binna', 'Rocha', 'BEED', '2026-08-24 20:30:35'),
+(4, 'Binnakiss', 'Viodor', 'BSIT', '2026-09-22 00:28:55'),
+(5, 'Binnakisses', 'Viodor', 'BSIT', '2026-09-22 00:32:25');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD PRIMARY KEY (`borrow_id`),
+  ADD KEY `fk_borrow_student` (`student_id`),
+  ADD KEY `fk_borrow_book` (`book_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `borrow`
+--
+ALTER TABLE `borrow`
+  MODIFY `borrow_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD CONSTRAINT `fk_borrow_book` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`),
+  ADD CONSTRAINT `fk_borrow_student` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,184 @@
+<?php
+
+$host = 'localhost';
+$db = 'it30b_lab_db';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    echo "Connection successful";
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+
+session_start();
+
+$section = $_GET['section'] ?? 'students';
+$action = $_GET['action'] ?? '';
+
+/* Fetch students */
+$students = [];
+
+if ($section === 'students') {
+    $stmt = $pdo->query("SELECT * FROM students ORDER BY student_id ASC");
+    $students = $stmt->fetchAll();
+}
+
+
+//Create Student
+if ($section == 'students' && $action === 'create') {
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        $firstName = trim($_POST['student_first_name'] ?? '');
+        $lastName = trim($_POST['student_last_name'] ?? '');
+        $course = trim($_POST['student_course'] ?? '');
+
+        if ($firstName !== '' && $lastName !== '' && $course !== '') {
+
+            $sql = "
+                INSERT INTO students(
+                    student_first_name,
+                    student_last_name,
+                    student_course
+                )
+                VALUES (?,?,?)
+            ";
+
+            $stmt = $pdo->prepare($sql);
+
+            $stmt->execute([
+                $firstName,
+                $lastName,
+                $course
+            ]);
+
+            header("Location: index.php?section=students");
+            exit;
+        }
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library System</title>
+</head>
+<body>
+
+<h1>Simple Library System</h1>
+
+<nav>
+    <a href="index.php?section=students">Students</a> |
+    <a href="index.php?section=books">Books</a> |
+    <a href="index.php?section=borrow">Borrow</a>
+</nav>
+<hr>
+
+<?php if ($section === 'students'): ?>
+    <h2>Students</h1>
+
+<p>
+    <a href="index.php?section=students&action=create">
+        add student
+    </a>
+</p>
+
+<?php if($action === 'create'): ?>
+    <h2>Create Student</h2>
+
+    <form method="POST">
+        <label>First Name:</label>
+        <br>
+        <input type="text"
+        name="student_first_name"
+         required
+
+         />
+</p>
+
+        <label>Last Name:</label>
+        <br>
+        <input type="text"
+        name="student_last_name"
+            required
+        />
+</p>
+
+        <label>Course:</label>
+        <br>
+        <input type="text"
+        name="student_course"
+            required
+
+        />
+</p>
+
+<button type="submit">
+    save
+</button>
+    <a href="index.php?section=students">
+        cancel
+</a>     
+<?php else: ?>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Course</th>
+                <th>Created At</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php foreach ($students as $student): ?>
+                <tr>
+                    <td><?= htmlspecialchars($student['student_id']) ?></td>
+                    <td><?= htmlspecialchars($student['student_first_name']) ?></td>
+                    <td><?= htmlspecialchars($student['student_last_name']) ?></td>
+                    <td><?= htmlspecialchars($student['student_course']) ?></td>
+                    <td><?= htmlspecialchars($student['student_created_at']) ?></td>
+                    <td>
+                        <a href="#">Edit</a>
+                        <a href="#">Delete</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+
+<?php endif; ?>
+        
+
+
+
+<?php endif; ?>
+
+<?php if ($section === 'books'): ?>
+    <h2>Books</h2>
+<?php endif; ?>
+
+<?php if ($section === 'borrow'): ?>
+    <h2>Borrow</h2>
+<?php endif; ?>
+
+</body>
+</html>
